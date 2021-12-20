c936e93dcf6c00293471e5a50f9d211f62b470c1
anandubale <au26199@gmail.com>
2021-12-20 16:23:17 +0530

    Added Displying Welcome

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeePresentOrNotRefactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Problems.");
+
+        }
+    }
+}
