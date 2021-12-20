6ccc6113e80b69cd12e1c25c1037513e36556f48
anandubale <au26199@gmail.com>
2021-12-20 16:33:48 +0530

    Added Refactored Progam which calculate employee wage

---
+++ DailyEmployeeWage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePresentOrNotRefactoring
+{
+    class DailyEmployeeWage
+    {
+        //Fields
+        int IsFullTime = 1;
+        int EmpRatePerHr = 200;
+
+        //Constructor
+
+        public DailyEmployeeWage(int FullTime)
+        {
+            this.IsFullTime = FullTime;
+        }
+
+
+        Random random = new Random();
+
+        //Methods
+        public void Solution()
+        {
+            int EmpHr = 0;
+            int EmpWage = 0;
+
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                EmpHr = 0;
+
+            }
+            else
+            {
+                EmpHr = 1;
+            }
+
+            EmpWage = EmpHr * EmpRatePerHr;
+            Console.WriteLine("Employee Wage is " + EmpWage);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,14 @@ namespace EmployeePresentOrNotRefactoring
         {
 
             Console.WriteLine("Welcome to Employee Wage Computation Problems.");
-            
+
+            /*
             PresentOrNot p1 = new PresentOrNot(1);
             p1.Solution();
+            */
+
+            DailyEmployeeWage W1 = new DailyEmployeeWage(1);
+            W1.Solution();
 
         }
     }
